52b343f9c856e316a3f0a64f518f8abc474539ea
Junk <unconfigured@null.spigotmc.org>
2023-11-02 13:44:59 +0100

    + Added Dummy class for CupidMatcher

---
+++ src/main/java/de/uni_marburg/schematch/matching/CupidMatcher.java
@@ -0,0 +1,13 @@
+package de.uni_marburg.schematch.matching;
+
+import de.uni_marburg.schematch.matchtask.tablepair.TablePair;
+
+/**
+ * TODO: Implement Cupid Matcher
+ */
+public class CupidMatcher extends Matcher{
+    @Override
+    public float[][] match(TablePair tablePair) {
+        return new float[0][];
+    }
+}
