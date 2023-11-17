dce0ef05d2864828ad409e9862cbc54f5710f2bd
Daniel Engelbach <danielengelbach@Daniels-Air.local.wenjo.de>
2023-11-17 12:26:30 +0100

    unique column detection

--- src/main/java/de/uni_marburg/schematch/boosting/CustomMatrixBoosting.java
+++ src/main/java/de/uni_marburg/schematch/boosting/CustomMatrixBoosting.java
@@ -1,13 +1,14 @@
 package de.uni_marburg.schematch.boosting;
 
 import de.uni_marburg.schematch.data.Scenario;
+import de.uni_marburg.schematch.data.Table;
 import de.uni_marburg.schematch.matching.Matcher;
 import de.uni_marburg.schematch.matching.TokenizedMatcher;
 import de.uni_marburg.schematch.matchtask.tablepair.TablePair;
 import lombok.Setter;
 
 import java.util.ArrayList;
-import java.util.Set;
+import java.util.HashMap;
 
 @Setter
 public class CustomMatrixBoosting implements SimMatrixBoosting {
@@ -25,11 +26,47 @@ public class CustomMatrixBoosting implements SimMatrixBoosting {
     static int c = 0;
     public void test(){
         if(c == 0){
+            ArrayList<Integer> unique = getUniqueColumns(this.tablePair.getTargetTable(), true);
+            System.out.println(unique);
+
+            /**
+            System.out.println("SOURCE DATA");
             for (int i = 0; i < this.tablePair.getSourceTable().getNumberOfColumns(); i++){
-                ArrayList<String> sourceTokens_i = (ArrayList<String>) this.tablePair.getSourceTable().getColumn(i).getValues();
-                System.out.println(sourceTokens_i);
+                System.out.println("COLUMN: " + this.tablePair.getSourceTable().getColumn(i).getLabel());
+                System.out.println("VALUES: " + this.tablePair.getSourceTable().getColumn(i).getValues());
             }
+
+            System.out.println("\nTARGET DATA");
+            for (int i = 0; i < this.tablePair.getTargetTable().getNumberOfColumns(); i++){
+                System.out.println("COLUMN: " + this.tablePair.getTargetTable().getColumn(i).getLabel());
+                System.out.println("VALUES: " + this.tablePair.getTargetTable().getColumn(i).getValues());
+            }
+             **/
         }
         c++;
+    }
+
+    private ArrayList<Integer> getUniqueColumns(Table table, boolean ignoreEmpty){
+        ArrayList<Integer> result = new ArrayList<>();
+        for (int i = 0; i < table.getNumberOfColumns(); i++){
+            ArrayList<String> values = (ArrayList<String>) table.getColumn(i).getValues();
+            HashMap<String, Integer> map = new HashMap<>();
+            boolean isUnique = true;
+            for (String value : values) {
+                if(value.isEmpty() && ignoreEmpty){
+                    continue;
+                }
+                if (map.get(value) != null) {
+                    isUnique = false;
+                    break;
+                } else {
+                    map.put(value, map.size());
+                }
+            }
+            if(isUnique){
+                result.add(i);
+            }
+        }
+        return result;
     }
 }
