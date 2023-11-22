e9c0802efbda5fb281ce6a7e98ca6adace6c012b
Fabian Mahling <FabianMahling@web.de>
2023-11-22 19:19:34 +0100

    dirty working version utilizing docker compose for embdi

--- src/main/java/de/uni_marburg/schematch/matching/sota/EmbdiMatcher.java
+++ src/main/java/de/uni_marburg/schematch/matching/sota/EmbdiMatcher.java
@@ -9,23 +9,44 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+import java.net.URI;
+import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
 @Data
 @EqualsAndHashCode(callSuper=false)
 @NoArgsConstructor
 public class EmbdiMatcher extends Matcher {
     Logger log = LogManager.getLogger(EmbdiMatcher.class);
+    private Integer serverPort;
+
 
     @Override
     public float[][] match(TablePair tablePair) {
-        log.info("Running Embdi matcher for tables '{}' as source and '{}' as target.",
+        log.info("Running EmbDI matcher for tables '{}' as source and '{}' as target.",
                 tablePair.getSourceTable().getName(), tablePair.getTargetTable().getName());
 
-        return PythonUtils.runPythonMatcher(
-                tablePair,
-                false,
-                "embdi",
-                "embdi.py",
-                "-i_1", tablePair.getSourceTable().getPath(),
-                "-i_2", tablePair.getTargetTable().getPath());
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder(URI.create(String.format(
+                    "http://127.0.0.1:%d/match?table1=%s&table2=%s",
+                    serverPort,
+                    URLEncoder.encode(tablePair.getSourceTable().pathRelativeToDataDirectory(), StandardCharsets.UTF_8),
+                    URLEncoder.encode(tablePair.getTargetTable().pathRelativeToDataDirectory(), StandardCharsets.UTF_8)
+            ))).build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            if (response.statusCode() != 200) {
+                getLogger().error("Running EmbDI matcher failed with status code {}", response.statusCode());
+                return tablePair.getEmptySimMatrix();
+            }
+            return PythonUtils.readMatcherOutput(Arrays.stream(response.body().split("\n")).toList(), tablePair);
+        } catch (Exception e) {
+            getLogger().error("Running EmdDI matcher failed with exception. Is the EmbDI server running?", e);
+            return tablePair.getEmptySimMatrix();
+        }
     }
 }
