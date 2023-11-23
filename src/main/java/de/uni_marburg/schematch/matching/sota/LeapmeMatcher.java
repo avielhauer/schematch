f189ba015fc7a60e6e97a7487235a0b08cd945a8
Fabian Mahling <FabianMahling@web.de>
2023-11-23 14:41:10 +0100

    Provide utility method for httpRequestGeneration & sending

--- src/main/java/de/uni_marburg/schematch/matching/sota/LeapmeMatcher.java
+++ src/main/java/de/uni_marburg/schematch/matching/sota/LeapmeMatcher.java
@@ -6,19 +6,11 @@ import de.uni_marburg.schematch.utils.PythonUtils;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.apache.commons.lang3.tuple.ImmutablePair;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.StringReader;
-import java.net.URI;
-import java.net.URLEncoder;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -33,16 +25,12 @@ public class LeapmeMatcher extends Matcher {
         getLogger().debug("Running LEAPME matcher for tables '{}' as source and '{}' as target.",
                 tablePair.getSourceTable().getPath(), tablePair.getTargetTable().getPath());
         try {
-            HttpClient client = HttpClient.newHttpClient();
-            HttpRequest request = HttpRequest.newBuilder(URI.create(String.format(
-                            "http://127.0.0.1:%d/match?prediction_used_info_types=%s&prediction_used_features=%s&table1=%s&table2=%s",
-                            serverPort,
-                            information,
-                            features,
-                            URLEncoder.encode(tablePair.getSourceTable().pathRelativeToDataDirectory(), StandardCharsets.UTF_8),
-                            URLEncoder.encode(tablePair.getTargetTable().pathRelativeToDataDirectory(), StandardCharsets.UTF_8)
-                    ))).build();
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            HttpResponse<String> response = PythonUtils.sendMatchRequest(serverPort, List.of(
+                    new ImmutablePair<>("table1", tablePair.getSourceTable().pathRelativeToDataDirectory()),
+                    new ImmutablePair<>("table2", tablePair.getTargetTable().pathRelativeToDataDirectory()),
+                    new ImmutablePair<>("prediction_used_info_types", information),
+                    new ImmutablePair<>("prediction_used_features", features)
+            ));
             if (response.statusCode() != 200) {
                 getLogger().error("Running LEAPME matcher failed with status code {}", response.statusCode());
                 return tablePair.getEmptySimMatrix();
