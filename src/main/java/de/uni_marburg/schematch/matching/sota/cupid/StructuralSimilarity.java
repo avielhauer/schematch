1c51d3370b61fb3a1571e3d1b9cd99f160c401d1
Lukas Junk <junk@students.uni-marburg.de>
2023-12-17 19:09:27 +0100

    + implemented StructuralSimilarity.computeSSim

--- src/main/java/de/uni_marburg/schematch/matching/sota/cupid/StructuralSimilarity.java
+++ src/main/java/de/uni_marburg/schematch/matching/sota/cupid/StructuralSimilarity.java
@@ -1,22 +1,37 @@
 package de.uni_marburg.schematch.matching.sota.cupid;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class StructuralSimilarity {
     public static double computeSSim(
             SchemaElementNode s,
             SchemaElementNode t,
-            HashMap<String,HashMap<StringPair,Double>> sims,
+            Map<String,Map<StringPair,Double>> sims,
             double th_accept) {
-        return Double.NaN;
+        List<SchemaElementNode> sLeaves = s.leaves();
+        List<SchemaElementNode> tLeaves = t.leaves();
+
+        if (sLeaves.size() > tLeaves.size()*2 || sLeaves.size()*2 < tLeaves.size()) return Double.NaN;
+
+        List<SchemaElementNode> sStrongLink = new ArrayList<SchemaElementNode>();
+        List<SchemaElementNode> tStrongLink = new ArrayList<SchemaElementNode>();
+
+        for (SchemaElementNode s1: sLeaves) {
+            for (SchemaElementNode t1: sLeaves) {
+                StringPair pair = new StringPair(s1.name,t1.name);
+                if (sims.get("wsim").get(pair) > th_accept) {
+                    sStrongLink.add(s1);
+                    tStrongLink.add(t1);
+                }
+            }
+        }
+        return (double) (sStrongLink.size() + tStrongLink.size()) / (sLeaves.size() + tLeaves.size());
     }
 
     public static void changeStructuralSimilarity(
             List<SchemaElementNode> sLeaves,
             List<SchemaElementNode> tLeaves,
-            HashMap<String, HashMap<StringPair, Double>> sims,
+            Map<String, Map<StringPair, Double>> sims,
             double cInc) {
         List<StringPair> allLeaves = product(sLeaves, tLeaves);
 
