a8ba2c9a29389079e3007b90c5cff3a5c2f778f3
Alexander Vielhauer <alexander.vielhauer@gmail.com>
2024-01-18 14:43:11 +0100

    Add tests for datatype cast

--- src/test/java/de/uni_marburg/schematch/data/metadata/DatatypeTest.java
+++ src/test/java/de/uni_marburg/schematch/data/metadata/DatatypeTest.java
@@ -5,6 +5,9 @@ import de.uni_marburg.schematch.data.Scenario;
 import de.uni_marburg.schematch.data.Table;
 import org.junit.jupiter.api.Test;
 
+import java.util.Date;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DatatypeTest {
@@ -21,7 +24,42 @@ class DatatypeTest {
         assertEquals(Datatype.FLOAT, Datatype.determineDatatype(Datatype.calculateScores(sourceTable.getColumn(3))));
         assertEquals(Datatype.BOOLEAN, Datatype.determineDatatype(Datatype.calculateScores(sourceTable.getColumn(4))));
         assertEquals(Datatype.BOOLEAN, Datatype.determineDatatype(Datatype.calculateScores(sourceTable.getColumn(5))));
-        //assertEquals(Datatype.DATE, Datatype.determineDatatype(Datatype.calculateScores(sourceTable.getColumn(6))));
-        //assertEquals(Datatype.GEO_LOCATION, Datatype.determineDatatype(Datatype.calculateScores(sourceTable.getColumn(7))));
+        assertEquals(Datatype.DATE, Datatype.determineDatatype(Datatype.calculateScores(sourceTable.getColumn(6))));
+        assertEquals(Datatype.GEO_LOCATION, Datatype.determineDatatype(Datatype.calculateScores(sourceTable.getColumn(7))));
+    }
+
+    @Test
+    void castDatatype() {
+        TestUtils.TestData testData = TestUtils.getTestData();
+        Scenario scenario = new Scenario(testData.getScenarios().get("test2").getPath());
+        Table sourceTable = scenario.getSourceDatabase().getTableByName("authors");
+
+        List<Integer> castedCol0 = Datatype.castToInt(sourceTable.getColumn(0));
+        for (Integer i : castedCol0) {
+            assertNotNull(i);
+        }
+        List<Float> castedCol3 = Datatype.castToFloat(sourceTable.getColumn(3));
+        for (Float f : castedCol3) {
+            assertNotNull(f);
+        }
+        List<Boolean> castedCol4 = Datatype.castToBoolean(sourceTable.getColumn(4));
+        for (Boolean b : castedCol4) {
+            assertNotNull(b);
+        }
+        List<Boolean> castedCol5 = Datatype.castToBoolean(sourceTable.getColumn(5));
+        for (Boolean b : castedCol5) {
+            assertNotNull(b);
+        }
+        List<Date> castedCol6 = Datatype.castToDate(sourceTable.getColumn(6));
+        for (Date d : castedCol6) {
+            assertNotNull(d);
+        }
+
+        Table targetTable = scenario.getTargetDatabase().getTableByName("authors");
+
+        List<Date> castedCol2Target = Datatype.castToDate(targetTable.getColumn(2));
+        assertNull(castedCol2Target.get(0));
+        assertNotNull(castedCol2Target.get(1));
+        assertNotNull(castedCol2Target.get(2));
     }
 }
