cbabce50f3a29ec3b70789d0819b3d36b959cabb
Anton Thiel <anton.thiel@turbinekreuzberg.com>
2024-01-30 19:12:49 +0100

    Add: "Booost" most connected pairs

--- src/main/java/de/uni_marburg/schematch/boosting/sf_algorithm/flooding/Flooder.java
+++ src/main/java/de/uni_marburg/schematch/boosting/sf_algorithm/flooding/Flooder.java
@@ -5,6 +5,8 @@ import de.uni_marburg.schematch.boosting.sf_algorithm.propagation_graph.Propagat
 import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import java.util.*;
+import java.util.function.Consumer;
 
 @RequiredArgsConstructor
 public abstract class Flooder {
@@ -13,6 +15,7 @@ public abstract class Flooder {
 
     public final float[][] flood(int maxIterations, float minResidualLength){
         this.pGraph.resetNodeSimilarity();
+        this.getMostConnectedPairs();
         for(int i = 0; i < maxIterations; i++){
             this.flooding_step();
             float sumSquaredDifference = 0;
@@ -29,6 +32,41 @@ public abstract class Flooder {
         }
 
         return pGraph.getSimMatrix();
+    }
+
+    private void getMostConnectedPairs(){
+        // Berechne Degrees von jeder PropagationNode
+        Collection listOfAllPropNodes = this.pGraph.getNodeList().values();
+        Map<PropagationNode,Integer> nodeDegreeMap = new HashMap<>();
+        for(Object pNode: listOfAllPropNodes){
+
+            nodeDegreeMap.put((PropagationNode) pNode,this.pGraph.degreeOf((PropagationNode) pNode));
+        }
+
+        // Erstelle eine Liste aus den Einträgen der Map
+        List<Map.Entry<PropagationNode, Integer>> entryList = new ArrayList<>(nodeDegreeMap.entrySet());
+
+        // Sortiere die Liste anhand der Anzahl an Kanten
+        entryList.sort(Map.Entry.comparingByValue());
+
+        // Erstelle Sublist aus dem 75% Quantil
+        entryList = entryList.subList(this.getIndexThirdQuartile(entryList), entryList.size());
+
+        // TODO: Is this the right way of boosting pairs initially?
+        Consumer<Map.Entry<PropagationNode, Integer>> addInitialBoost = value -> value.getKey().setInitialSim(0.5F);
+        entryList.stream().forEach(addInitialBoost);
+        entryList.get(0);
+    }
+
+    /**
+     *
+     * @param sortedList
+     * @return index of third quartile of given list
+     */
+    private Integer getIndexThirdQuartile(List<Map.Entry<PropagationNode, Integer>> sortedList){
+        int listSize = sortedList.size();
+        Double p = 0.75;
+        return (int) Math.ceil(p * listSize) - 1;
     }
 
     protected abstract void flooding_step();
