fix feature distance calculation

src/main/resources/embedAlign/embedAlign_cache.py
@@ -64,18 +64,22 @@ class RepresentationCache:
         )
 
     def get_filtered_sm(self, source_table, target_table):
-        source_indices = [i for i, node in enumerate(self.source_table_nodes_sorted_as_given) if extract_table(node) == source_table]
-        target_indices = [i for i, node in enumerate(self.target_table_nodes_sorted_as_given) if extract_table(node) == target_table]
-
+        source_indices_lookup = {node: i for i, node in enumerate(self.source_table_nodes_sorted_as_given) if extract_table(node) == source_table}
+        target_indices_lookup = {node: i for i, node in enumerate(self.target_table_nodes_sorted_as_given) if extract_table(node) == target_table}
+        filtered_original_source_column_nodes = [node for node in self.source_all_original_table_nodes_sorted_as_given if extract_table(node) == source_table]
+        filtered_original_target_column_nodes = [node for node in self.target_all_original_table_nodes_sorted_as_given if extract_table(node) == target_table]
         sm = np.zeros(
             (
-                len(source_indices),
-                len(target_indices),
+                len(filtered_original_source_column_nodes),
+                len(filtered_original_target_column_nodes),
             )
         )
 
-        for sm_i, csr_i in enumerate(source_indices):
-            for sm_j, csr_j in enumerate(target_indices):
-                sm[sm_i][sm_j] = self.csr_k_similar_sm.getrow(csr_i).getcol(csr_j).toarray()[0][0]
+        for sm_i, source_node in enumerate(filtered_original_source_column_nodes):
+            for sm_j, target_node in enumerate(filtered_original_target_column_nodes):
+                if source_node in source_indices_lookup and target_node in target_indices_lookup:
+                    sm[sm_i][sm_j] = self.csr_k_similar_sm.getrow(source_indices_lookup[source_node]).getcol(target_indices_lookup[target_node]).toarray()[0][0]
+                else:
+                    sm[sm_i][sm_j] = -404.0
 
         return sm

src/main/resources/embedAlign/embedAlign_graph.py
@@ -23,6 +23,7 @@ def extract_table(node_label):
 def load_feature_dict(path):
     with open(path, "r") as fp:
         dict = json.loads(json.load(fp))
+    dict = {table: {column: np.asarray(features) for column, features in columns.items()} for table, columns in dict.items()}
     return dict
 
 
@@ -86,15 +87,20 @@ class EmbedGraph:
             for value in values.values():
                 return len(value)
 
+    def get_features_shape(self):
+        for table in self.feature_dict.values():
+            for features in table.values():
+                return features.shape
+
     def get_features(self):
         lookup = {}
-        empty_dict_feature_list = [
+        empty_dict_feature_list = np.asarray([
             0.0 for _ in range(self.default_feature_vector_length_dict)
-        ]
+        ])
         for i, k in enumerate(NODE_TYPES):
             feature = [0.0 for _ in range(len(NODE_TYPES))]
             feature[i] = 1.0
-            lookup[k] = feature
+            lookup[k] = np.asarray(feature)
         encodings = []
         for node in self.graph.nodes:
             type_one_hot_encodings = lookup[extract_node_type(node)]
@@ -103,6 +109,35 @@ class EmbedGraph:
                 features = self.feature_dict[extract_table(node)][extract_column(node)]
             else:
                 features = empty_dict_feature_list
-            encodings.append(type_one_hot_encodings + features)
+            encodings.append(np.concatenate((type_one_hot_encodings, features)))
 
         return np.asarray(encodings)
+
+    def normalize_features(self, other_graph):
+        assert self.get_features_shape() == other_graph.get_features_shape()
+        min_feature_values = np.full_like(self.get_features_shape(), np.inf, dtype=float)
+        max_feature_values = np.full_like(self.get_features_shape(), np.NINF, dtype=float)
+        for tables in list(self.feature_dict.values()) + list(other_graph.feature_dict.values()):
+            for feature in tables.values():
+                min_feature_values = np.minimum(min_feature_values, feature)
+                max_feature_values = np.maximum(max_feature_values, feature)
+
+        scale_features = max_feature_values - min_feature_values
+        scale_features[scale_features == 0.0] = 1.0
+        scale_features = 1.0 / scale_features
+
+        self.feature_dict = {
+            table: {
+                column: (features - min_feature_values) * scale_features
+                for column, features in columns.items()
+            }
+            for table, columns in self.feature_dict.items()
+        }
+
+        other_graph.feature_dict = {
+            table: {
+                column: (features - min_feature_values) * scale_features
+                for column, features in columns.items()
+            }
+            for table, columns in other_graph.feature_dict.items()
+        }

src/main/resources/embedAlign/xnetmf.py
@@ -139,8 +139,10 @@ def get_features(graph, rep_method, verbose = True):
 def compute_similarity(graph, rep_method, vec1, vec2, node_attributes = None, node_indices = None):
     dist = rep_method.gammastruc * np.linalg.norm(vec1 - vec2) #compare distances between structural identities
     if graph.node_attributes is not None:
-        #distance is number of disagreeing attributes
-        attr_dist = np.sum(graph.node_attributes[node_indices[0]] != graph.node_attributes[node_indices[1]])
+        # previousle (in REGAL), distance is number of disagreeing attributes
+        # as we use real world values, we instead use the euclidean distance, as also proposed in the REGAL paper
+        # prev: attr_dist = np.sum(graph.node_attributes[node_indices[0]] != graph.node_attributes[node_indices[1]])
+        attr_dist = np.sqrt(np.sum((graph.node_attributes[node_indices[0]] - graph.node_attributes[node_indices[1]])**2))
         dist += rep_method.gammaattr * attr_dist
     return np.exp(-dist) #convert distances (weighted by coefficients on structure and attributes) to similarities
 