38d8337546612d9bb7bb9d6826f8a02148001f91
Fabian Mahling <FabianMahling@web.de>
2024-02-19 13:31:48 +0100

    working evaluation script, giving a overview over performance

--- scripts/evaluate_runs.py
+++ scripts/evaluate_runs.py
@@ -27,6 +27,8 @@ def extract_properties(matcher_string):
     matcher_name = matcher_string[:matcher_string.find("(")]
     pattern = r'(\w+)\s*=\s*([^\s:]+)'
     matches = re.findall(pattern, matcher_string)
+    if matches:
+        matches[-1] = (matches[-1][0], matches[-1][1][:-1])  # remove closing bracket
     return matcher_name, dict(matches)
 
 
@@ -88,24 +90,31 @@ def extract_score(csv_path, performance, benchmark_dict):
         for i, matcher in enumerate(row1):
             if i == 0: # skip row labels
                 continue
-            benchmark_dict.setdefault(matcher, {})[performance] = float(row2[i])
+            benchmark_dict.setdefault("_performances", {}).setdefault(matcher, {})[performance] = float(row2[i])
+
 def import_benchmark(root_path):
     benchmarks = {}
+    for performance_filename, performance_name in PERFORMANCES.items():
+        performance_csv = os.path.join(root_path, "_performances", performance_filename,
+                                       "performance_overview_line1.csv")
+        extract_score(performance_csv, performance_name, benchmarks)
     for dataset in os.listdir(root_path):
         dataset_path = os.path.join(root_path, dataset)
-        if os.path.isdir(dataset_path) and dataset != "_performances" and dataset.startswith("ISIA"):
+        if os.path.isdir(dataset_path) and dataset != "_performances":
             dataset_benchmarks = {}
+            for performance_filename, performance_name in PERFORMANCES.items():
+                performance_csv = os.path.join(dataset_path, "_performances", performance_filename,
+                                       "performance_overview_line1.csv")
+                extract_score(performance_csv, performance_name, dataset_benchmarks)
             # Iterate over scenarios
             for scenario in os.listdir(dataset_path):
                 scenario_benchmarks = {}
                 scenario_path = os.path.join(dataset_path, scenario)
                 if not os.path.isdir(scenario_path) or scenario == "_performances":
                     continue
-                # Now you can perform operations within each scenario
                 print("Dataset:", dataset)
                 print("Scenario:", scenario)
-                # Add your processing logic here
-                outputs_dir = os.path.join(scenario_path, "_outputs", "MatchingStepLine1")
+                # outputs_dir = os.path.join(scenario_path, "_outputs", "MatchingStepLine1")
                 # for matcher in os.listdir(outputs_dir):
                 #    process_output_file(os.path.join(outputs_dir, matcher), dataset, scenario, matcher)
                 for performance_filename, performance_name in PERFORMANCES.items():
@@ -164,48 +173,62 @@ def visualize_performance(benchmarks):
 
 def generalize_benchmark_matchers(benchmarks):
     generalized_benchmark = {}
-    for dataset, scenario_dict in benchmarks.items():
-        new_scenario_dict = {}
-        for scenario, matcher_dict in scenario_dict.items():
-            new_matcher_dict = {}
-            for matcher_string, performance in matcher_dict.items():
+    for level, level_dict in benchmarks.items():
+        new_level_dict = {}
+        if level == "_performances":
+            for matcher_string, performance in level_dict.items():
                 matcher_name, props = extract_properties(matcher_string)
-                new_matcher_dict.setdefault(matcher_name, []).append(dict(**{"props": props}, **performance))
-            new_scenario_dict[scenario] = new_matcher_dict
-        generalized_benchmark[dataset] = new_scenario_dict
+                new_level_dict.setdefault(matcher_name, []).append(dict(**{"props": props}, **performance))
+        else:
+            new_level_dict = generalize_benchmark_matchers(level_dict)
+        generalized_benchmark[level] = new_level_dict
     return generalized_benchmark
 
 
-def plot_dataset_evaluation(scenario_dict):
-    num_scenarios = len(scenario_dict)
+def plot_dataset_evaluation(matcher_to_performances, title=None, path_to_save=None, performances_to_be_plotted=PERFORMANCES):
+    num_matchers = len(matcher_to_performances)
     num_columns=2
     fig, axs = plt.subplots(
-        num_scenarios // num_columns+1, num_columns, figsize=(12, 8), sharey=True
+        len(performances_to_be_plotted) // num_columns+1, num_columns, figsize=(12, 8), sharey=False
     )
 
-    for idx, (scenario, ax) in enumerate(zip(scenario_dict.keys(), axs.flatten()), start=1):
+    if title:
+        fig.suptitle(title)
+    for idx, ((performance_name, performance_id), ax) in enumerate(zip(performances_to_be_plotted.items(), axs.flatten()), start=1):
         x_labels = []
         y = []
-        for matcher, performances in scenario_dict[scenario].items():
+        for matcher, performances in matcher_to_performances.items():
+            if matcher.endswith("Matcher"):
+                matcher = matcher[:-7]
             x_labels.append(matcher)
-            y.append(max([performance["F1"] for performance in performances]))
+            y.append(max([performance[performance_id] for performance in performances]))
+        ax.set_title(performance_name)
         ax.bar(x_labels, y)
         ax.tick_params(axis='x', labelrotation=90)
-    plt.tight_layout()
-    plt.show()
+    plt.subplots_adjust(hspace=2.0)
+    plt.savefig(os.path.join(path_to_save, "performance.png"), dpi=300)
+    #plt.show()
 
 
 def get_matchers(generalized_benchmark):
-    for scenario_dict in generalized_benchmark.values():
-        for runs in scenario_dict.values():
-            return list(set(runs.keys()))
-
+    return generalized_benchmark["_performances"].keys()
 
 if __name__ == "__main__":
     root_path = sys.argv[1]
     benchmarks = import_benchmark(root_path)
+    print(benchmarks)
     generalized_benchmarks = generalize_benchmark_matchers(benchmarks)
-    matchers = get_matchers(generalized_benchmarks)
+    print(generalized_benchmarks)
 
+    plot_dataset_evaluation(generalized_benchmarks["_performances"], "Overall Performance", root_path)
     for dataset, scenario_dict in generalized_benchmarks.items():
-        plot_dataset_evaluation(scenario_dict, matchers)
+        if dataset == "_performances":
+            continue
+        plot_dataset_evaluation(scenario_dict["_performances"], dataset, os.path.join(root_path, dataset))
+        for scenario, performance_dict in scenario_dict.items():
+            if scenario == "_performances":
+                continue
+            plot_dataset_evaluation(performance_dict["_performances"], scenario, os.path.join(root_path, dataset, scenario))
+
+    #for dataset, scenario_dict in generalized_benchmarks.items():
+    #    plot_dataset_evaluation(scenario_dict)
