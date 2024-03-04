483b9954308fad9e9198c47084262e452c54479c
Fabian Mahling <FabianMahling@web.de>
2024-03-04 22:01:04 +0100

    make evaluator work for semi complete mappings

--- src/main/java/de/uni_marburg/schematch/evaluation/Evaluator.java
+++ src/main/java/de/uni_marburg/schematch/evaluation/Evaluator.java
@@ -32,15 +32,18 @@ public class Evaluator {
     private List<Integer> sourceGroundTruthIndices;
     private List<Integer> targetGroundTruthIndices;
     private int[][] transposedGroundTruthMatrix;
+    private List<List<Integer>> sourceTransformationLookup;
+    private List<List<Integer>> targetTransformationLookup;
     private Pair<Integer, Integer>[][] groundTruthTransformationIndexLookup;
 
     public Evaluator(List<Metric> metrics, Scenario scenario, int[][] groundTruthMatrix) {
         this.metrics = metrics;
         this.scenario = scenario;
-        this.groundTruthMatrix = groundTruthMatrix;
-        if(scenario.getDataset().isDenormalized()){
-            reconstructOriginalGroundTruth();
+        if (scenario.getDataset().isDenormalized()) {
+            groundTruthMatrix =  reconstructOriginalGroundTruth();
         }
+        this.groundTruthMatrix = groundTruthMatrix;
+
         this.groundTruthVector = ArrayUtils.flattenMatrix(groundTruthMatrix);
         this.numGroundTruth = ArrayUtils.sumOfMatrix(groundTruthMatrix);
 
@@ -59,21 +62,21 @@ public class Evaluator {
         }
     }
 
-    private Map<String, Map<String, Integer>> importMapping(Path filePath, Database database){
-        Map<String, Map<String, Integer>> mapping = new HashMap<>();
+    private Map<String, Map<String, List<Integer>>> importMapping(Path filePath, Database database) {
+        Map<String, Map<String, List<Integer>>> mapping = new HashMap<>();
         try {
-            for(String line : Files.readAllLines(filePath)){
+            for (String line : Files.readAllLines(filePath)) {
                 String[] parts = line.split(" = ");
                 String[] denormalizedParts = parts[0].split("\\.");
-                String[] originalParts= parts[1].split("\\.");
+                String[] originalParts = parts[1].split("\\.");
 
                 Table denormalizedTable = database.getTableByName(denormalizedParts[0]);
                 Column denormalizedColumn = denormalizedTable.getColumnByName(denormalizedParts[1]);
                 int gtOffset = denormalizedTable.getOffset() + denormalizedTable.getColumns().indexOf(denormalizedColumn);
 
-                Map<String, Integer> innerMap = mapping.computeIfAbsent(originalParts[0], k -> new HashMap<>());
-
-                innerMap.put(originalParts[1], gtOffset);
+                mapping.computeIfAbsent(originalParts[0], k -> new HashMap<>())
+                        .computeIfAbsent(originalParts[1], k -> new ArrayList<>())
+                        .add(gtOffset);
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -82,32 +85,31 @@ public class Evaluator {
         return mapping;
     }
 
-    private int getNumberAttributes(Map<String, Map<String, Integer>> map) {
+    private int getNumberAttributes(Map<String, Map<String, List<Integer>>> map) {
         int totalCount = 0;
-        for (Map<String, Integer> innerMap : map.values()) {
+        for (Map<String, List<Integer>> innerMap : map.values()) {
             totalCount += innerMap.size();
         }
         return totalCount;
     }
 
-    private Map<String, Integer> getNewPositionLookup(Map<String, Map<String, Integer>> mapping){
+    private Map<String, Integer> getNewPositionLookup(Map<String, Map<String, List<Integer>>> mapping) {
         Map<String, Integer> lookup = new HashMap<>();
         int i = 0;
-        for (Map.Entry<String, Map<String, Integer>> outerEntry : mapping.entrySet()) {
+        for (Map.Entry<String, Map<String, List<Integer>>> outerEntry : mapping.entrySet()) {
             String outerKey = outerEntry.getKey();
-            Map<String, Integer> innerMap = outerEntry.getValue();
+            Map<String, List<Integer>> innerMap = outerEntry.getValue();
 
-            for (Map.Entry<String, Integer> innerEntry : innerMap.entrySet()) {
+            for (Map.Entry<String, List<Integer>> innerEntry : innerMap.entrySet()) {
                 String innerKey = innerEntry.getKey();
-                Integer value = innerEntry.getValue();
-                lookup.put(outerKey + "." +  innerKey, i);
+                lookup.put(outerKey + "." + innerKey, i);
                 i++;
             }
         }
         return lookup;
     }
 
-    private Collection<Pair<String, String>> importOriginalMappings(Path pathToOriginalGroundTruth){
+    private Collection<Pair<String, String>> importOriginalMappings(Path pathToOriginalGroundTruth) {
         List<Pair<String, String>> mappings = new ArrayList<>();
 
         try {
@@ -124,63 +126,82 @@ public class Evaluator {
         return mappings;
     }
 
-    private void reconstructOriginalGroundTruth(){
+    private int[][] reconstructOriginalGroundTruth() {
         Configuration config = Configuration.getInstance();
-        Map<String, Map<String, Integer>> sourceMappings = importMapping(Paths.get(scenario.getPath(), config.getDefaultGroundTruthDir(), "source_mapping.csv"), scenario.getSourceDatabase());
-        Map<String, Map<String, Integer>> targetMappings = importMapping(Paths.get(scenario.getPath(), config.getDefaultGroundTruthDir(), "target_mapping.csv"), scenario.getTargetDatabase());
+        Map<String, Map<String, List<Integer>>> sourceMappings = importMapping(Paths.get(scenario.getPath(), config.getDefaultGroundTruthDir(), "source_mapping.csv"), scenario.getSourceDatabase());
+        Map<String, Map<String, List<Integer>>> targetMappings = importMapping(Paths.get(scenario.getPath(), config.getDefaultGroundTruthDir(), "target_mapping.csv"), scenario.getTargetDatabase());
         int sourceN = getNumberAttributes(sourceMappings);
         int targetN = getNumberAttributes(targetMappings);
         Map<String, Integer> sourcePositionLookup = getNewPositionLookup(sourceMappings);
         Map<String, Integer> targetPositionLookup = getNewPositionLookup(targetMappings);
         Collection<Pair<String, String>> originalMatchings = importOriginalMappings(Paths.get(scenario.getPath(), config.getDefaultGroundTruthDir(), "actual_ground_truth.txt"));
 
-        groundTruthMatrix = new int[sourceN][targetN];
-        for(Pair<String, String> originalMatch : originalMatchings) {
-            groundTruthMatrix[sourcePositionLookup.get(originalMatch.getLeft())][targetPositionLookup.get(originalMatch.getRight())] = 1;
+        int[][] reconstructedGroundTruthMatrix = new int[sourceN][targetN];
+        for (Pair<String, String> originalMatch : originalMatchings) {
+            Integer sourceIndex = sourcePositionLookup.get(originalMatch.getLeft());
+            Integer targetIndex = targetPositionLookup.get(originalMatch.getRight());
+            if(sourceIndex == null){
+                log.warn("Could not find " + originalMatch.getLeft() + " in  sourcePositionLookup.");
+                continue;
+            }
+            if(targetIndex == null){
+                log.warn("Could not find "+ originalMatch.getRight()+" in targetPositionLookup.");
+                continue;
+            }
+            reconstructedGroundTruthMatrix[sourcePositionLookup.get(originalMatch.getLeft())][targetPositionLookup.get(originalMatch.getRight())] = 1;
         }
 
-        groundTruthTransformationIndexLookup = new Pair[sourceN][targetN];
 
-        generateTransformationIndexLookup(sourceMappings, targetMappings);
-
+        generateTransformationIndexLookup(sourceMappings, sourcePositionLookup, sourceN, targetMappings, targetPositionLookup, targetN);
+        return reconstructedGroundTruthMatrix;
     }
 
-    private void generateTransformationIndexLookup(Map<String, Map<String, Integer>> sourceMappings, Map<String, Map<String, Integer>> targetMappings) {
-        int i = 0;
-        for (Map.Entry<String, Map<String, Integer>> sourceTableEntry : sourceMappings.entrySet()) {
-            Map<String, Integer> sourceColumnsEntry = sourceTableEntry.getValue();
+    private void generateTransformationIndexLookup(Map<String, Map<String, List<Integer>>> sourceMappings, Map<String, Integer> sourcePositionLookup, Integer sourceN, Map<String, Map<String, List<Integer>>> targetMappings, Map<String, Integer> targetPositionLookup, Integer targetN) {
+        sourceTransformationLookup = buildLookup(sourceMappings, sourcePositionLookup, sourceN);
+        assert(sourcePositionLookup.size() == groundTruthMatrix.length);
+        targetTransformationLookup = buildLookup(targetMappings, targetPositionLookup, targetN);
+        assert(targetTransformationLookup.size() == groundTruthMatrix[0].length);
+    }
 
-            for (Map.Entry<String, Integer> sourceColumn : sourceColumnsEntry.entrySet()) {
-                Integer sourceOriginalIndex = sourceColumn.getValue();
-                int j = 0;
-                for (Map.Entry<String, Map<String, Integer>> targetTableEntry : targetMappings.entrySet()) {
-                    Map<String, Integer> targetColumnsEntry = targetTableEntry.getValue();
 
-                    for (Map.Entry<String, Integer> targetColumn : targetColumnsEntry.entrySet()) {
-                        Integer targetOriginalIndex = targetColumn.getValue();
-                        groundTruthTransformationIndexLookup[i][j] = new ImmutablePair<>(sourceOriginalIndex, targetOriginalIndex);
-                        j++;
-                    }
-                }
-                i++;
+    private List<List<Integer>> buildLookup(Map<String, Map<String, List<Integer>>> mappings, Map<String, Integer> positionLookup, Integer N) {
+        List<List<Integer>> lookup = new ArrayList<>();
 
-            }
+        for (int i = 0; i < N; i++) {
+            lookup.add(new ArrayList<>());
         }
+
+        for (Map.Entry<String, Integer> sourcePosition : positionLookup.entrySet()) {
+            String columnID = sourcePosition.getKey();
+            String table = columnID.split("\\.")[0];
+            String column = columnID.split("\\.")[1];
+            Integer posInTransformedMatrix = sourcePosition.getValue();
+            lookup.set(posInTransformedMatrix, mappings.get(table).get(column));
+        }
+
+        return lookup;
     }
 
     private float[][] reconstructOriginalSimMatrix(float[][] simMatrix) {
         float[][] newSimMatrix = new float[groundTruthMatrix.length][groundTruthMatrix[0].length];
-        for(int i = 0 ; i < newSimMatrix.length; i++){
-            for(int j = 0; j < newSimMatrix[0].length; j++){
-                Pair<Integer, Integer> pos = groundTruthTransformationIndexLookup[i][j];
-                newSimMatrix[i][j] = simMatrix[pos.getLeft()][pos.getRight()];
+        for (int i = 0; i < sourceTransformationLookup.size(); i++) {
+            for (int j = 0; j < targetTransformationLookup.size(); j++) {
+                List<Integer> sourceLookups = sourceTransformationLookup.get(i);
+                List<Integer> targetLookups = targetTransformationLookup.get(j);
+                float max = 0.0f;
+                for(Integer sourceLookup : sourceLookups){
+                    for(Integer targetLookup: targetLookups){
+                        max = Math.max(max, simMatrix[sourceLookup][targetLookup]);
+                    }
+                }
+                newSimMatrix[i][j] = max;
             }
         }
         return newSimMatrix;
     }
 
     public Map<Metric, Performance> evaluate(float[][] simMatrix) {
-        if(scenario.getDataset().isDenormalized()){
+        if (scenario.getDataset().isDenormalized()) {
             simMatrix = reconstructOriginalSimMatrix(simMatrix);
         }
         float[] simVector = ArrayUtils.flattenMatrix(simMatrix);
