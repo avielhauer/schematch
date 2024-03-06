a6f390123e62b281bd028ad9d9a3a83484c57cf5
Lukas Junk <junk@students.uni-marburg.de>
2024-03-06 15:14:39 +0100

    + set treshold to .7

--- src/main/java/de/uni_marburg/schematch/evaluation/metric/FScore.java
+++ src/main/java/de/uni_marburg/schematch/evaluation/metric/FScore.java
@@ -4,13 +4,13 @@ public class FScore extends Metric {
     @Override
     public float run(int[] groundTruthVector, float[] simVector) {
         int factor = 1;
-        float threshold = 0.0f;
+        float threshold = 0.7f;
         int tp = 0;
         int fp = 0;
         int tn = 0;
         int fn = 0;
         for (int i = 0; i < groundTruthVector.length; i++) {
-            boolean isPositive = simVector[i] > threshold;
+            boolean isPositive = simVector[i] >= threshold;
             if (groundTruthVector[i] == 1) {
                 if (isPositive) {
                     tp++;
--- src/main/java/de/uni_marburg/schematch/evaluation/metric/Recall.java
+++ src/main/java/de/uni_marburg/schematch/evaluation/metric/Recall.java
@@ -3,13 +3,13 @@ package de.uni_marburg.schematch.evaluation.metric;
 public class Recall extends Metric {
     @Override
     public float run(int[] groundTruthVector, float[] simVector) {
-        float threshold = 0.0f;
+        float threshold = 0.7f;
         int tp = 0;
         int fp = 0;
         int tn = 0;
         int fn = 0;
         for (int i = 0; i < groundTruthVector.length; i++) {
-            boolean isPositive = simVector[i] > threshold;
+            boolean isPositive = simVector[i] >= threshold;
             if (groundTruthVector[i] == 1) {
                 if (isPositive) {
                     tp++;
