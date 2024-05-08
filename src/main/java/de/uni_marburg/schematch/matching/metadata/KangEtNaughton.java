d0607b5f4bcb0466ef2233b78d8da0916d0cb082
Fabian Mahling <FabianMahling@web.de>
2024-05-08 19:20:20 +0200

    Refactor Kang et Naughton implementation, renaming and cleanup.

--- src/main/java/de/uni_marburg/schematch/matching/metadata/KangEtNaughton.java
+++ src/main/java/de/uni_marburg/schematch/matching/metadata/KangEtNaughton.java
@@ -10,11 +10,12 @@ import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-@EqualsAndHashCode(callSuper=false)
+@EqualsAndHashCode(callSuper = false)
 @NoArgsConstructor
 public class KangEtNaughton extends TablePairMatcher {
     final private int numSeededRuns = 100;
     final private double normalizedDistanceAlpha = 2.0;
+
     @Override
     public float[][] match(TablePair tablePair) {
 
@@ -22,6 +23,8 @@ public class KangEtNaughton extends TablePairMatcher {
         Table sourceTable = tablePair.getSourceTable();
         Table targetTable = tablePair.getTargetTable();
 
+        // We swap tables, so that the sourceTable contains more attributes, which makes the latter alignment code
+        // simpler. Note that the final sm need to be swapped back, so that the original order is respected.
         boolean swapped = false;
         if (targetTable.getColumns().size() > sourceTable.getColumns().size()) {
             Table temp = sourceTable;
@@ -30,19 +33,19 @@ public class KangEtNaughton extends TablePairMatcher {
             swapped = true;
         }
 
-        double[][] sourceEntropyMatrix = getEntropyMatrix(sourceTable);
-        double[][] targetEntropyMatrix = getEntropyMatrix(targetTable);
+        double[][] sourceMutualInformationMatrix = getMutualInformationMatrix(sourceTable);
+        double[][] targetMutualInformationMatrix = getMutualInformationMatrix(targetTable);
 
-        return HC(sourceEntropyMatrix, targetEntropyMatrix, swapped);
+        return HillClimbing(sourceMutualInformationMatrix, targetMutualInformationMatrix, swapped);
     }
 
-    float[][] HC(double[][] sourceEntropyMatrix, double[][] targetEntropyMatrix, boolean swapped){
+    float[][] HillClimbing(double[][] sourceEntropyMatrix, double[][] targetEntropyMatrix, boolean swapped) {
         double highestDistanceScore = Double.MIN_VALUE;
         List<Integer> minAlignment = new ArrayList<>();
         Random random = new Random(42);
-        for(int i = 0; i < numSeededRuns; i++){
+        for (int i = 0; i < numSeededRuns; i++) {
             Pair<List<Integer>, Double> runResult = runSeededHC(sourceEntropyMatrix, targetEntropyMatrix, random);
-            if(runResult.getRight() > highestDistanceScore){
+            if (runResult.getRight() > highestDistanceScore) {
                 minAlignment = runResult.getLeft();
                 highestDistanceScore = runResult.getRight();
             }
@@ -50,40 +53,41 @@ public class KangEtNaughton extends TablePairMatcher {
         return buildSimilarityMatrix(minAlignment, sourceEntropyMatrix, targetEntropyMatrix, swapped);
     }
 
-    float[][] buildSimilarityMatrix(List<Integer> minAlignment, double[][] sourceEntropyMatrix, double[][] targetEntropyMatrix, boolean swapped){
+    float[][] buildSimilarityMatrix(List<Integer> minAlignment, double[][] sourceMutualInformationMatrix, double[][] targetMutualInformationMatrix, boolean swapped) {
         float[][] sm;
-        if(swapped){
-            sm = new float[targetEntropyMatrix.length][sourceEntropyMatrix.length];
-            for(int i = 0; i < minAlignment.size(); i++){
-                if(minAlignment.get(i) != -1){
+        if (swapped) {
+            sm = new float[targetMutualInformationMatrix.length][sourceMutualInformationMatrix.length];
+            for (int i = 0; i < minAlignment.size(); i++) {
+                if (minAlignment.get(i) != -1) {
                     sm[minAlignment.get(i)][i] = 1.0f;
                 }
             }
-        }else{
-            sm = new float[sourceEntropyMatrix.length][targetEntropyMatrix.length];
-            for(int i = 0; i < minAlignment.size(); i++){
-                if(minAlignment.get(i) != -1){
+        } else {
+            sm = new float[sourceMutualInformationMatrix.length][targetMutualInformationMatrix.length];
+            for (int i = 0; i < minAlignment.size(); i++) {
+                if (minAlignment.get(i) != -1) {
                     sm[i][minAlignment.get(i)] = 1.0f;
                 }
             }
         }
         return sm;
     }
-    Pair<List<Integer>, Double> runSeededHC(double[][] sourceEntropyMatrix, double[][] targetEntropyMatrix, Random random){
+
+    Pair<List<Integer>, Double> runSeededHC(double[][] sourceEntropyMatrix, double[][] targetEntropyMatrix, Random random) {
         List<Integer> alignments = getInitialAlignments(sourceEntropyMatrix.length, targetEntropyMatrix.length, random);
         double highestDistanceScore = calculateDistance(sourceEntropyMatrix, targetEntropyMatrix, alignments);
         boolean changed = true;
-        while(changed){
+        while (changed) {
             changed = false;
             List<Integer> newAlignments = null;
-            for(int i = 0; i < alignments.size(); i ++){
-                for(int j = i + 1; j < alignments.size(); j++){
+            for (int i = 0; i < alignments.size(); i++) {
+                for (int j = i + 1; j < alignments.size(); j++) {
                     List<Integer> permutedAlignment = new ArrayList<>(alignments);
                     Integer temp = permutedAlignment.get(i);
                     permutedAlignment.set(i, permutedAlignment.get(j));
                     permutedAlignment.set(j, temp);
                     double permutedDistance = calculateDistance(sourceEntropyMatrix, targetEntropyMatrix, permutedAlignment);
-                    if(permutedDistance > highestDistanceScore){ // Normal distance metric -> the higher, the "better"
+                    if (permutedDistance > highestDistanceScore) { // Normal distance metric -> the higher, the "better"
                         highestDistanceScore = permutedDistance;
                         newAlignments = permutedAlignment;
                         changed = true;
@@ -91,7 +95,7 @@ public class KangEtNaughton extends TablePairMatcher {
 
                 }
             }
-            if(changed){
+            if (changed) {
                 alignments = newAlignments;
             }
         }
@@ -100,9 +104,9 @@ public class KangEtNaughton extends TablePairMatcher {
 
     private double calculateDistance(double[][] sourceEntropyMatrix, double[][] targetEntropyMatrix, List<Integer> alignments) {
         double normalDistance = 0;
-        for(int i = 0; i < sourceEntropyMatrix.length; i++){
-            for(int j = 0; j < sourceEntropyMatrix.length; j++){
-                if(alignments.get(i) == -1 || alignments.get(j) == -1){ // TODO: verify this is fine.
+        for (int i = 0; i < sourceEntropyMatrix.length; i++) {
+            for (int j = 0; j < sourceEntropyMatrix.length; j++) {
+                if (alignments.get(i) == -1 || alignments.get(j) == -1) { // TODO: verify this is fine.
                     continue;
                 }
                 double a_ij = sourceEntropyMatrix[i][j];
@@ -113,53 +117,44 @@ public class KangEtNaughton extends TablePairMatcher {
         return normalDistance;
     }
 
-    List<Integer> getInitialAlignments(int sourceSize, int targetSize, Random random){
+    List<Integer> getInitialAlignments(int sourceSize, int targetSize, Random random) {
         List<Integer> alignments = IntStream.range(0, sourceSize).boxed().collect(Collectors.toList());
-        for(int i = targetSize; i < sourceSize; i++){
+        for (int i = targetSize; i < sourceSize; i++) {
             alignments.set(i, -1); // no alignment when source and target are of unequal size (we assume bijective)
         }
         Collections.shuffle(alignments, random);
         return alignments;
     }
-    double[][] getEntropyMatrix(Table table){
+
+    double[][] getMutualInformationMatrix(Table table) {
         double[][] entropyMatrix = new double[table.getNumColumns()][table.getNumColumns()];
 
-        for(int i = 0; i < table.getNumColumns(); i++){
-            for(int j = 0; j < table.getNumColumns() && j <= i; j++){
-                if(i == j){
-                    entropyMatrix[i][j] = getEntropy(combineColumns(table.getColumn(i).getValues(), table.getColumn(j).getValues()));
-                }
-                else{
-                    entropyMatrix[j][i] = getMutualInformation(combineColumns(table.getColumn(i).getValues(), table.getColumn(j).getValues()));
-                    entropyMatrix[i][j] = getMutualInformation(combineColumns(table.getColumn(i).getValues(), table.getColumn(j).getValues()));
-                }
+        for (int i = 0; i < table.getNumColumns(); i++) {
+            for (int j = 0; j < table.getNumColumns() && j <= i; j++) {
+                double mutualInformation = getMutualInformation(table.getColumn(i).getValues(), table.getColumn(j).getValues());
+                entropyMatrix[j][i] = mutualInformation;
+                entropyMatrix[i][j] = mutualInformation;
             }
         }
         return entropyMatrix;
     }
 
-    List<List<String>> combineColumns(List<String> column1, List<String> column2){
-        assert(column1.size() == column2.size());
-        List<List<String>> combinedColumns = new ArrayList<>(column1.size());
-        for(int i = 0; i < column1.size(); i++){
-            combinedColumns.add(List.of(column1.get(i), column2.get(i)));
-        }
-        return combinedColumns;
-    }
+    private double getMutualInformation(final List<String> columnValuesX, final List<String> columnValuesY) {
 
-    private double getMutualInformation(final List<List<String>> values){
-        int total = values.size();
-
+        assert (columnValuesX.size() == columnValuesY.size());
+        int total = columnValuesX.size();
+        // Count occurrences of X, Y, and XY.
         Map<String, Integer> frequencyCounterX = new HashMap<>();
         Map<String, Integer> frequencyCounterY = new HashMap<>();
         Map<List<String>, Integer> frequencyCounterXY = new HashMap<>();
-        for (List<String> value : values) {
-            frequencyCounterX.put(value.get(0), frequencyCounterX.getOrDefault(value.get(0), 0) + 1);
-            frequencyCounterY.put(value.get(1), frequencyCounterY.getOrDefault(value.get(1), 0) + 1);
-            frequencyCounterXY.put(value, frequencyCounterXY.getOrDefault(value, 0 ) + 1);
+        for (int i = 0; i < total; i++) {
+            frequencyCounterX.put(columnValuesX.get(i), frequencyCounterX.getOrDefault(columnValuesX.get(i), 0) + 1);
+            frequencyCounterY.put(columnValuesY.get(i), frequencyCounterY.getOrDefault(columnValuesY.get(i), 0) + 1);
+            List<String> combination = List.of(columnValuesX.get(i), columnValuesY.get(i));
+            frequencyCounterXY.put(combination, frequencyCounterXY.getOrDefault(combination, 0) + 1);
         }
 
-        // Calculate entropy
+        // Calculate Mutual Information based on P_X, P_Y, and P_XY.
         double mutualInformation = 0.0;
         for (Map.Entry<List<String>, Integer> entry : frequencyCounterXY.entrySet()) {
             double probabilityXY = (double) entry.getValue() / total;
@@ -169,24 +164,6 @@ public class KangEtNaughton extends TablePairMatcher {
         }
 
         return mutualInformation;
-    }
-
-    private double getEntropy(final List<List<String>> values){
-        int total = values.size();
-
-        Map<List<String>, Integer> frequencyCounter = new HashMap<>();
-        for (List<String> value : values) {
-            frequencyCounter.put(value, frequencyCounter.getOrDefault(value, 0) + 1);
-        }
-
-        // Calculate entropy
-        double entropy = 0.0;
-        for (Map.Entry<List<String>, Integer> entry : frequencyCounter.entrySet()) {
-            double probability = (double) entry.getValue() / total;
-            entropy -= probability * Math.log(probability) / Math.log(2);
-        }
-
-        return entropy;
     }
 
 }
